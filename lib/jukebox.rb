4320ae37d8b55c77c09d79294b84e50b7de6c06e
Johnnathan Baxter <jbaxter6@radford.edu>
2020-05-29 15:54:56 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -42,13 +42,13 @@ end
 
 def run
 puts "Please enter a command:"
-
+  input = gets.strip
   until input == "exit" do
-    input = gets.strip
+    
     if input == "list"
-      list
+      list(array)
       elsif input == "play"
-      play
+      play(array)
       elsif input == "help"
       help
     end
